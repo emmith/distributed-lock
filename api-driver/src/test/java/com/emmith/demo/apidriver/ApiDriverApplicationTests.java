048684290675200b8dfe49e64fdcb5d24a3b5d5f
wangrenjun <18873890185@163.com>
2022-06-20 22:41:26 +0800

    添加司机模块基本骨架

---
+++ api-driver/src/test/java/com/emmith/demo/apidriver/ApiDriverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.emmith.demo.apidriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootTest
+class ApiDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
